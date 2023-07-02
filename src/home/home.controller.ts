c4f7d7cee0b1b0a750e67d6af7360ac4bcbb8cff
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-07-02 18:13:42 +0300

    Homes, crud operations

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,4 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
+import { HomeService } from './home.service';
+import { HomeResponseDto } from './dto/home.dto';
 
 @Controller('home')
-export class HomeController {}
+export class HomeController {
+  constructor(private readonly homeService: HomeService) {}
+  @Get()
+  async getHomes(): Promise<HomeResponseDto[]> {
+    return this.homeService.getHomes();
+  }
+
+  @Get(':id')
+  getHome(@Param('id') id: string): string {
+    return 'Home ' + id;
+  }
+
+  @Post()
+  createHome(): string {
+    return 'Create Home';
+  }
+
+  @Put(':id')
+  updateHome(@Param('id') id: string): string {
+    return 'Update Home ' + id;
+  }
+
+  @Delete(':id')
+  deleteHome(@Param('id') id: string): string {
+    return 'Delete Home ' + id;
+  }
+}
--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { HomeResponseDto } from './dto/home.dto';
 
 @Injectable()
-export class HomeService {}
+export class HomeService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async getHomes(): Promise<HomeResponseDto[]> {
+    const response = await this.prismaService.home.findMany({
+      select: {
+        id: true,
+        address: true,
+        number_of_bedrooms: true,
+        number_of_bathrooms: true,
+        city: true,
+        price: true,
+        images: {
+          select: {
+            url: true,
+          },
+        },
+      },
+      take: 3,
+    });
+    return response.map((home) => {
+      const fetchHome = { ...home, image: home.images[0].url };
+      delete fetchHome.images;
+      return new HomeResponseDto(fetchHome);
+    });
+  }
+
+  async getHome(id: string): Promise<string> {
+    return 'Home ' + id;
+  }
+
+  async createHome(): Promise<string> {
+    return 'Create Home';
+  }
+
+  async updateHome(id: string): Promise<string> {
+    return 'Update Home ' + id;
+  }
+
+  async deleteHome(id: string): Promise<string> {
+    return 'Delete Home ' + id;
+  }
+}
